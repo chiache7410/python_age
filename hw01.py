f3ed6a907ff45a4fe8dedf5530a7e1996ea454b6
chiache Lee <chiache7410@gmail.com>
2021-07-22 15:16:21 +0800

    v2

--- hw01.py
+++ hw01.py
@@ -1,12 +1,11 @@
 passwd = 'a123456'
-input()
-passwdin = 'a'
-input()
 count = 3
-input()
-while passwdin != passwd
-    passwdin = input('請輸入密碼?')
-    count = count - 1
-    print('密碼錯誤 還有', count, '次機會')
-print('登入成功')
+while count > 0:
+    pwd = input('請輸入密碼?')
+    if pwd == passwd:
+        print('登入成功')
+        break
+    else:
+        count = count - 1
+        print('密碼錯誤 還有', count, '次機會')
 input()
